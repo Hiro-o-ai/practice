074c464a5726751114de8f0c55cab2518ba74731
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-05-05 21:43:46 +0900

    [finish]search

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "books#top"
   get "home/about", to: "books#about", as: "about_home"
+  get "search", to: "searches#search"
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
