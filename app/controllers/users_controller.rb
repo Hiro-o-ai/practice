262006e61bcc831d0b10aa55d650064e0f9f70c5
vagrant <vagrant@localhost.localdomain>
2020-05-08 04:15:26 +0000

    dig

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image, :postcode, :prefecture_name, :address_city, :address_street, :address_building)
   end
   def correct_user
     @user = User.find(params[:id])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_102543) do
+ActiveRecord::Schema.define(version: 2020_05_08_035326) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -56,6 +56,11 @@ ActiveRecord::Schema.define(version: 2020_05_04_102543) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "profile_image_id"
+    t.integer "postcode"
+    t.integer "prefecture_code"
+    t.string "address_city"
+    t.string "address_street"
+    t.string "address_building"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
